539bb74e37dc28d513d4514b9dcbbea07949b69c
JeetKini <151983803+JeetKini@users.noreply.github.com>
2023-12-27 18:15:13 +0530

    Add files via upload

---
+++ day2/App_Start/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VidlysProject.Dtos;
+using VidlysProject.Models;
+
+namespace VidlysProject.App_Start
+{
+    public class MappingProfile :Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Customer, CustomerDto>();
+            CreateMap<CustomerDto, Customer>();
+            CreateMap<MembershipType, MembershipTypeDto>();
+            CreateMap<Movie, MoviesDto>();
+            CreateMap<MoviesDto, Movie>();
+            CreateMap<MovieGenre, MovieGenreDto>();
+
+        }
+    }
+}
---
+++ day2/Dtos/MovieGenreDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VidlysProject.Dtos
+{
+    public class MovieGenreDto
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+    }
+}
